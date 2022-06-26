45c11032e8951c1a0945f505bc232f58f70f7280
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2022-06-25 22:29:09 -0300

    feat: Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    label: PropTypes.string,
+    labelClassname: PropTypes.string,
+};
+
+type InputProps = React.HTMLProps<HTMLInputElement> & PropTypes.InferProps<typeof propTypes>;
+
+const Input: React.FC<InputProps> = props => {
+    const {
+        label,
+        labelClassname,
+        id = '',
+        ...inputProps
+    } = props;
+
+    return (
+        <div>
+            {label &&
+                <label
+                    htmlFor={id}
+                    className={`form-label ${labelClassname || ''}`}
+                >
+                    {label}
+                </label>
+            }
+            <div className='input-group mb-3'>
+                <input
+                    id={id}
+                    className={`form-control ${inputProps?.className || ''}`}
+                    {...inputProps}
+                />
+            </div>
+        </div>
+    );
+}
+
+Input.propTypes = propTypes;
+
+export default Input;
